b92989fec3ff2c3b3623fd6627b6e0bd68cd9aff
Toby Clemson <tobyclemson@gmail.com>
2017-06-19 14:32:51 +0100

    Add signing key details.

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "Simplify reflection of annotations on Java types."
   :url "http://github.com/tobyclemson/exegesis"
   :license {:name "The MIT License"
-            :url "https://opensource.org/licenses/MIT"}
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :source-paths      ["src/clojure"]
-  :test-paths        ["test/clojure"]
+  :source-paths ["src/clojure"]
+  :test-paths ["test/clojure"]
   :java-source-paths ["src/java" "test/java"]
-  :deploy-repositories [["releases" {:url :clojars
-                                     :creds :gpg}]])
+  :deploy-repositories [["releases" {:url     :clojars
+                                     :creds   :gpg
+                                     :signing {:gpg-key "44CE9D82"}}]])
